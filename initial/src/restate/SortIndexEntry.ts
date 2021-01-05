c5eb5cb924771bf7918c87e087733cbf355c3450
arista <arista@abramsons.info>
2021-01-05 06:33:39 -0500

    remove the 'initial' set of source code

--- initial/src/restate/SortIndexEntry.ts
+++
@@ -1,6 +0,0 @@
-import {Entity} from "./Entity"
-import {EntityState} from "./EntityState"
-
-export class SortIndexEntry<E extends Entity> {
-  constructor(public entity: EntityState<E>, public sortValues: Array<any>) {}
-}
