961992f45e4eb6da6173702512548e967f1b354a
arista <arista@abramsons.info>
2021-03-25 06:21:39 -0400

    created blank gh-pages branch

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-;; Used by emacs to prevent it from creating the temporary lockfiles
-;; that break the React development server or other watch-based processes
-((nil . ((create-lockfiles . nil))))
--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "trailingComma": "es5",
-  "semi": false,
-  "bracketSpacing": false
-}
